d2f1521c1afbe8dd6b7a37f8fd708313bfc52450
adpa1982 <adpa363@hotmail.com>
2020-08-31 22:58:46 -0500

    Fin seccion 19, despliegue

--- index.js
+++ index.js
@@ -31,7 +31,11 @@ app.use( '/api/medicos', require('./routes/medicos') );
 app.use( '/api/todo', require('./routes/busquedas') );
 app.use( '/api/upload', require('./routes/uploads') );
 
-
+// Acceso a la url de angular
+// const path = require('path');
+/*app.get('*', (req, res) => {
+    res.sendFile( path.resolve(__dirname, 'public/index.html') );
+});*/
 
 
 app.listen(process.env.PORT, () => {
